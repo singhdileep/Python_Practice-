87c12c486ed579e1294d0456f863e3351a871054
Dileep singh <rajsingh@Dileeps-MacBook-Pro.local>
2020-07-20 22:43:49 +0530

    first commit

---
+++ file.py
@@ -0,0 +1,30 @@
+
+'''W+ means read and write if the file is not exist ,if it is existing then it will be override'''
+
+
+# with open("abc.txt","w+") as f:
+#     f.write("Hi My name is Dileep Singh\n")
+#     f.write("I am from Deoria \n")
+#     f.write("I am working in Accenture \n")
+#     f.seek(0)
+#     data=f.read()
+# print(data)
+
+
+'''a+ means read and append and it wont be override if the file is exists.'''  
+
+
+# with open("abc.txt","a+") as f:
+#     f.write("I have 2+ year experience in Accenture")
+#     f.seek(0)
+#     data=f.read()
+# print(data)    
+
+
+# with open("abc.txt","a+") as f:
+#     f.write("I have 2+ year experience in Accenture")
+#     f.seek(0)
+#     data=f.read()
+# print(data)    
+
+.....    nnnnn 
