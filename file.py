d5286175a2780a69e7f49e36bd77d9d82dbf091a
Dileep singh <rajsingh@Dileeps-MacBook-Pro.local>
2020-07-21 23:25:01 +0530

    Second commit till zip file

--- file.py
+++ file.py
@@ -1,5 +1,13 @@
 
-'''W+ means read and write if the file is not exist ,if it is existing then it will be override'''
+'''W+ means read and write if the file is not exist ,if it is existing then it will be override
+   git init
+   git add .
+   git commit -m "first commit" 
+   git remote add origin https://github.com/singhdileep/Python_Practice-.git
+   git push -u origin master
+
+
+'''
 
 
 # with open("abc.txt","w+") as f:
@@ -21,10 +29,113 @@
 # print(data)    
 
 
-# with open("abc.txt","a+") as f:
-#     f.write("I have 2+ year experience in Accenture")
-#     f.seek(0)
-#     data=f.read()
-# print(data)    
+'''Program to count the occurence of the word from the file '''
 
-.....    nnnnn 
+# with open("abc.txt","r") as f:    
+#     wordcount={}
+#     for word in f.read().split():
+#         if word not in wordcount:
+#             wordcount[word] = 1
+#         else:
+#             wordcount[word] += 1
+#     print (word,wordcount)
+
+
+'''Write a program to check whether the given file exists or not. If it is available then print its content.
+   To check file is exist or not we shoud use isfile() function along with os library and its module and its submodue
+   os.path.isfile(fname)
+'''
+
+# import os,sys
+# fname=input("Enter the file name:")
+# if os.path.isfile("abc.txt"):
+#     print("File is exists",fname)
+#     f=open(fname,"r")
+# else:
+#     print("File not exists",fname)
+#     sys.exit()
+# print("The content of the file is :")
+# data=f.read()
+# print(data)
+
+
+
+'''Code to print no of line ,words and characters present in file '''
+
+
+# f=open("abc.txt","r")
+# lcount=wcount=ccount=0
+# for line in f:
+#     lcount=lcount+1
+#     ccount=ccount+len(line)
+#     word=line.split()
+#     wcount=wcount+len(word)
+# print("The required Details Below")
+# print("------------------------------")
+# print("The count of Line :",lcount)    
+# print("The count of character :",ccount)    
+# print("The count of Words :",wcount)    
+
+
+'''CSV file - It means comma seperated values 
+   Create CSV file 
+'''
+
+
+# import csv
+# with open("emp.csv","w",newline='') as f:
+#    w=csv.writer(f)
+#    w.writerow(["Eno","Ename","Esal","Eaddr"])
+#    n=int(input("Enter the number of Emplyees: "))
+#    for i in range(n):
+#       eno=input("Enter the Employee No :")
+#       ename=input("Enter the Employee Name :")
+#       esal=input("Enter the Employee Salary :")
+#       eaddr=input("Enter the Employee Address :")
+#       w.writerow([eno,ename,esal,eaddr])
+# print("The total Employees data written to csv is Successfully")
+
+
+
+'''Reading data from CSV file'''
+
+# import csv
+# with open("emp.csv","r") as f:
+#    data=csv.reader(f)
+#    d=list(data)
+#    for line in d:
+#       for word in line:
+#          print(word,"\t",end='') 
+#       print()
+
+
+'''Zipping and UnZipping file
+
+The main Advantages of Zip file is that
+It improves memory Utilization 
+We can reduce the transport time
+We can improves performances.
+synax of zip file is 
+f=ZipFile("abc.zip","w","ZIP_DEFLATED")
+For that we have to import zip module
+'''
+
+# from zipfile import *
+# f=ZipFile("file.zip",'w',ZIP_DEFLATED)
+# f.write("abc.txt")
+# f.write("emp.csv")
+# # f.write("file3.txt")
+# # f.write("file4.txt")
+# f.close()
+# print("Zip file is created Successfully")
+
+'''UnZipping the file '''
+
+# from zipfile import *
+# f=ZipFile("file.zip",'r',ZIP_STORED)
+# names=f.namelist()
+# for name in names:
+#    print("File name is ",name)
+#    f1=open(name,'r')
+#    print(f1.read())
+#    print()
